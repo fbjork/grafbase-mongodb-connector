96e5e555e51d0bfa3de06629988c363a4ce41ad2
Fredrik Björk <fredrik.bjork@gmail.com>
2023-08-30 13:57:46 +0200

    upgrade sdk

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,9 +1,8 @@
 import { g, config, connector } from '@grafbase/sdk'
 
-const mongo = connector.MongoDB({
-  name: 'blog',
+const mongo = connector.MongoDB('blog', {
+  url: g.env('MONGODB_API_URL'),
   apiKey: g.env('MONGODB_API_KEY'),
-  appId: g.env('MONGODB_API_ID'),
   dataSource: g.env('MONGODB_DATA_SOURCE'),
   database: g.env('MONGODB_DATABASE'),
 })
@@ -13,6 +12,6 @@ mongo.model('Post', {
   slug: g.string()
 }).collection('posts')
 
-g.datasource(mongo, { namespace: 'blog' })
+g.datasource(mongo)
 
 export default config({ schema: g })
